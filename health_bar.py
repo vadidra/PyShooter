892105bfe23842b86a7e880c34226e5d7a07d74c
vadidra <vadimdra@yahoo.com>
2024-06-30 21:07:24 -0700

    temp files removed

---
+++ health_bar.py
@@ -0,0 +1,21 @@
+import pygame
+
+from settings import screen, BLACK, RED, GREEN
+
+
+class HealthBar():
+	def __init__(self, x, y, health, max_health):
+		self.x = x
+		self.y = y
+		self.health = health
+		self.max_health = max_health
+
+	def draw(self, health):
+		# update with new health
+		self.health = health
+		# calculate health ratio
+		ratio = self.health / self.max_health
+		pygame.draw.rect(screen, BLACK, (self.x - 2, self.y - 2, 154, 24))
+		pygame.draw.rect(screen, RED, (self.x, self.y, 150, 20))
+		pygame.draw.rect(screen, GREEN, (self.x, self.y, 150 * ratio, 20))
+
